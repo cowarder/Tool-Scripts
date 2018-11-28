ba2cca7402c4c429d9cd0a26b2b5d502950f4b57
cowarder <1615482151@qq.com>
2018-11-28 17:31:39 +0800

    SubMultiWords.py

---
+++ NLP/SubMultiWords.py
@@ -0,0 +1,31 @@
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+
+mispell_dict = {'colour':'color',
+                'centre':'center',
+                'didnt':'did not',
+                'doesnt':'does not',
+                'isnt':'is not',
+                'shouldnt':'should not',
+                'favourite':'favorite',
+                'travelling':'traveling',
+                'counselling':'counseling',
+                'theatre':'theater',
+                'cancelled':'canceled',
+                'labour':'labor',
+                'organisation':'organization',
+                'wwii':'world war 2',
+                'citicise':'criticize',
+                'instagram': 'social medium',
+                'whatsapp': 'social medium',
+                'snapchat': 'social medium'
+
+                }
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
